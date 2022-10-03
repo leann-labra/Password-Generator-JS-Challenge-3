9f36c71b9ce5fa03011e32cd9058bef3d6a4bae9
leann-labra <leann.labra@gmail.com>
2022-10-03 00:45:28 -0700

    made some headway with prompts

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,27 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// declared variable selects id generator to trigger function
-//var char = '0123456789!@#$%^&.*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//var passwordLength = 8-128;
-//if (
- // char = '0123456789!@#$%^&.*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-
-
+var password = {
+  upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lower: 'abcdefghijklmnopqrstuvwxyz',
+  
+}
 // Write password to the #password input
 function writeRandomPassword() {
-  let char = '0123456789!@#$%^&.*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var userInput = window.prompt("Password length? (enter value between (8-128)")
 
-  return char[Math.floor(Math.random () * char.length)]
+  //first ask user the length
+  if(isNaN(passwordLength)) {
+    window.alert('Hey! That is not a number')
+    return;
+  } else 
+  if (passwordLength < 8 || passwordLength > 128 ) {
+    window.alert("Must choose a password length between values 8-128")
+    return;
+  } else {
+    return;
+  }
+
+  var userInput = window.prompt("uppercase or lowercase?")
+
+  
+
+  
+
+
+  
 }
 
-  var password = generatePassword(); //called method 
-  
+function generatePassword() {
+  var password = generatePassword()
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writeRandomPassword);
+
